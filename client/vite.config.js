c4129c45644da74386c4690cb3d90cee6e949407
Naveen <bhukyanaveennayak225@gmail.com>
2024-12-01 14:12:40 +0000

    Update vite.config.js configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This is the folder where Vite will output the build
+  },
 })
